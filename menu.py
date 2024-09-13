6119ac03e999d7a6a8a1706652c90f88af2280d7
Sherwin Fu <shangyi.fu@student.education.wa.edu.au>
2024-09-13 12:47:23 +0800

    Added the start of the code, and losing conditions

---
+++ menu.py
@@ -0,0 +1,5 @@
+import random
+
+print("The Monty Hall problem has confused people for decades.")
+print("In the game show, Let's Make a Deal, Monty Hall asks you to guess which closed door a prize is behind. ")
+print("You will need to choose A door, one of them has a car behind it and the rest having a goat behind it.")
--- partone.py
+++ partone.py
@@ -1,1 +1,9 @@
-print('samir sheik mydheeen')
+import random
+print('Choose: Door 1, Door 2, or Door 3')
+doorchoice=int(input('Choose a Door: '))
+windoor=(random.randint(1,3))
+if windoor == doorchoice:
+    print('Sorry, but there was a goat in door',doorchoice)
+    exit()
+else:
+    print('There was a goat in door' )
