83134aea37a072f0f6b3af9f0fa87257c9a85f68
Sherwin Fu <shangyi.fu@student.education.wa.edu.au>
2024-09-14 20:55:05 +0800

    Finished random car placement, started on User door

--- partone.py
+++ partone.py
@@ -2,5 +2,15 @@ import random
 door1 = 'goat'
 door2 = 'goat'
 door3 = 'goat'
-doornumber = random.randint(0,3)
-print(doornumber)
+strings = ["door1", "door2", "door3"]
+randselect = random.choice(strings)
+globals()[randselect]='car'
+print(door1,door2,door3)
+
+print('There are three doors, Door1, Door2 and Door 3.')
+userselect = str(input('Input 1 to choose Door1, 2 to choose Door2, 3 to choose Door3 or any other key to quit'))
+
+if userselect in ("1", "2", "3"):
+    
+    
+    
