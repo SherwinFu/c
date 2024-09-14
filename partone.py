e0854ed263b609717973a34341bbeb24114e9bc2
Sherwin Fu <shangyi.fu@student.education.wa.edu.au>
2024-09-14 19:59:58 +0800

    restart

--- partone.py
+++ partone.py
@@ -1,10 +1,1 @@
 import random
-print('Choose: Door 1, Door 2, or Door 3')
-doorchoice=int(input('Choose a Door: '))
-windoor=(random.randint(1,3))
-if windoor == doorchoice:
-    print('Sorry, but there was a goat in door',doorchoice)
-    exit()
-else:
-    print('You did not choose the losing door','door',windoor,'and door',doorchoice,'are left.')
-    print('Would you like to switch your door or stay on your current door, door ',doorchoice')
