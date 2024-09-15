680cfbc913b9cf64b0dd1d143c34a9deca536edf
Sherwin Fu <shangyi.fu@student.education.wa.edu.au>
2024-09-15 20:22:05 +0800

    Finished Table

--- partone.py
+++ partone.py
@@ -1,47 +1,78 @@
 from termcolor import cprint, colored
 import random
-door1 = 'goat'
-door2 = 'goat'
-door3 = 'goat'
-strings = ["door1", "door2", "door3"]
-randselect = random.choice(strings)
-globals()[randselect]='car'
-print(door1,door2,door3)
+x = 1
+choice = ""
+action = ""
+outcome = ""
+table=[['Round','Choice','Action','Outcome']]
 
-
-userselect = str(input(colored('Input door1 to choose Door1, door2 to choose Door2, door3 to choose Door3 or any other key to quit ','yellow')))
-
-cprint(colored('Round #1','yellow'))
-if userselect in ("door1", "door2", "door3"):
-    print('continue')
-else:
-    print('Thanks for Playing, Good Bye.')
-    exit()
+loop= 3
+while loop >0:
+    door1 = 'goat'
+    door2 = 'goat'
+    door3 = 'goat'
     
+    strings = ["door1", "door2", "door3"]
+    randselect = random.choice(strings)
+    globals()[randselect]='car'
+    print(door1,door2,door3)
+    loop = loop - 1
 
-stringremoved = [s for s in strings if s != userselect]
-goatselect = random.choice(stringremoved)
-print(stringremoved)
-if globals()[goatselect] == 'car':
-    stringremoved = [s for s in strings if s != goatselect]
-    goatselect = random.choice(stringremoved)
 
-else:
-    print(' ')
-print(goatselect,globals()[goatselect])
-        
-userselect2=input('Stay or Switch, w to switch, s to stay, and any other key to quit.')
-if userselect2 == 's':
-    if globals()[userselect] == 'car':
-        print('You Switched..... You Win!')
+    userselect = str(input(colored('Input door1 to choose Door1, door2 to choose Door2, door3 to choose Door3 or any other key to quit ','yellow')))
+    choice = userselect
+    cprint(colored('Round #1','yellow'))
+    if userselect in ("door1", "door2", "door3"):
+        print('continue')
     else:
-        print('You Switched..... You Lose!')
-elif userselect2 == 'w':
-        stay=print(globals()[userselect])
-        if globals()[userselect] == 'goat':
-            print('You Stayed..... You Win!')
-        else:
-            print('You Stayed..... You Lose!')
+        print('Thanks for Playing, Good Bye.')
+        exit()
+        
 
-else:
-    print('Thanks for Playing, Good Bye.')
+    stringremoved = [s for s in strings if s != userselect]
+    goatselect = random.choice(stringremoved)
+    print(stringremoved)
+    if globals()[goatselect] == 'car':
+        stringremoved = [s for s in strings if s != goatselect]
+        goatselect = random.choice(stringremoved)
+
+    else:
+        print(' ')
+    print(goatselect,globals()[goatselect])
+            
+    userselect2=input('Stay or Switch, switch to switch, stay to stay, and any other key to quit.')
+    action = userselect2
+
+    if userselect2 == 'switch':
+        if globals()[userselect] == 'goat':
+            print('You switched..... You Win!')
+            outcome = 'win'
+        else:
+            print('You Switched..... You Lose!')
+            outcome = 'lose'
+
+    elif userselect2 == 'stay':
+            stay=print(globals()[userselect])
+            if globals()[userselect] == 'car':
+                print('You Stayed..... You Win!')
+                outcome = 'win'
+
+            else:
+                print('You Stayed..... You Lose!')
+                outcome = 'lose'
+
+
+    else:
+        print('Thanks for Playing, Good Bye.')
+
+    listtable=['Round'+str(x),choice,action,outcome]
+    table.append(listtable)
+    x = x + 1
+
+
+
+
+
+
+for row in table:
+    print(row)
