807b68d4096efd2556e92853cfd5a76a123d2802
Sherwin Fu <shangyi.fu@student.education.wa.edu.au>
2024-09-18 16:03:57 +0800

    Added Percentages

--- partone.py
+++ partone.py
@@ -1,13 +1,18 @@
 from termcolor import cprint, colored
 import random
-c = 1
+staywin = 1
+switchwin = 1
+totalswitch = 1
+totalstay = 1
 x = 1
+c = 1
 choice = ""
 action = ""
 outcome = ""
 table=[['Round','Choice','Action','Outcome']]
-loopcontinue = "y"
-while loopcontinue == "y":
+
+loop= 3
+while loop >0:
     door1 = 'goat'
     door2 = 'goat'
     door3 = 'goat'
@@ -15,16 +20,19 @@ while loopcontinue == "y":
     strings = ["door1", "door2", "door3"]
     randselect = random.choice(strings)
     globals()[randselect]='car'
-    cprint(colored('Round #'+(str(c)),'yellow'))
+    loop = loop - 1
+
+    cprint(colored('Round #'+str(c),'yellow'))
+    c = c + 1
     userselect = str(input(colored('Input door1 to choose Door1, door2 to choose Door2, door3 to choose Door3 or any other key to quit ','yellow')))
     choice = userselect
-    c = c + 1
+       
     if userselect in ("door1", "door2", "door3"):
         pass
     else:
-        loopcontinue = "n"
         print('Thanks for Playing, Good Bye.')
-        break
+        exit()
+        
 
     stringremoved = [s for s in strings if s != userselect]
     goatselect = random.choice(stringremoved)
@@ -33,35 +41,42 @@ while loopcontinue == "y":
         goatselect = random.choice(stringremoved)
 
     else:
-        pass     
+        print(colored({goatselect,globals()[goatselect]},'green'))
+            
     userselect2=input(colored('Stay or Switch, switch to switch, stay to stay, and any other key to quit.','cyan'))
     action = userselect2
 
     if userselect2 == 'switch':
         if globals()[userselect] == 'goat':
             print('You switched..... You Win!')
-            print(' ')
+            switchwin = switchwin + 1
+            totalswitch = totalswitch + 1
             outcome = 'win'
+            print(' ')
+
         else:
             print('You Switched..... You Lose!')
-            print(' ')            
+            totalswitch = totalswitch + 1
             outcome = 'lose'
+            print(' ')
 
     elif userselect2 == 'stay':
             stay=print(globals()[userselect])
             if globals()[userselect] == 'car':
                 print('You Stayed..... You Win!')
-                print(' ')
+                totalstay = totalstay + 1
+                staywin = staywin + 1
                 outcome = 'win'
+                print(' ')
 
             else:
                 print('You Stayed..... You Lose!')
-                print(' ')
+                staywin = staywin + 1
                 outcome = 'lose'
+                print(' ')
 
 
     else:
-        loopcontinue = "n"
         print('Thanks for Playing, Good Bye.')
 
     listtable=['Round'+str(x),choice,action,outcome]
@@ -75,3 +90,8 @@ while loopcontinue == "y":
 
 for row in table:
     print(row)
+staywinpercent = (staywin/totalstay*100)
+switchwinpercent = (switchwin/totalswitch*100)
+print("Total Win's From Staying",staywinpercent,"%")
+print("Total Win's From Switching",switchwinpercent,"%")
+
